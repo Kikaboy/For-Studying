0bb8a511ac9f6f9caecbc75bc33d2c926e3a9b5e
KirillPachin <pkirill.74@gmail.com>
2019-10-14 16:43:01 +0500

    1st version of commit

---
+++ Classwork2/Classwork2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Classwork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Input("n");
+            Console.WriteLine("Значение выражения" + F(n));
+            Console.ReadKey();
+
+        }
+        static bool F(double n)
+        {
+            return (n % 4 == 0 & n%10!=0);
+
+        }
+        static double Input(string Name)
+        {
+            Console.WriteLine("Введите число " + Name);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Classwork3/Classwork3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classwork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var x = Input("x");
+           var y = Input("y");
+            Console.WriteLine("Координаты:" + Coordinates(x,y));
+            Console.ReadKey();
+        }
+        static bool Coordinates(double x, double y)
+        {
+            bool v = (y > -0.5 & y < 2);
+            return (x > -1 & x < 1.5) & v;
+        }
+        static double Input(string Name)
+        {
+            Console.WriteLine("Введите значение " + Name);
+            return double.Parse(Console.ReadLine());
+        }
+
+    }
+}
