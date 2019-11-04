d4aea081aaff2179e26338639ef3e14182549ce1
Kikaboy <pkirill.74@gmail.com>
2019-11-04 14:36:48 +0500

    1st, 2nd, 3rd

---
+++ Homework3(9)/Homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Main1
+{
+    class Main1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение Х");
+            double X = double.Parse(Console.ReadLine());
+            double Y = (Math.Abs(1 + Math.Sqrt(X + 1) / (Math.Abs(1 - Math.Sqrt(X - 1)))));
+            Console.WriteLine("У = {0}", Y);
+            Console.ReadKey();
+
+        }
+    }
+}
